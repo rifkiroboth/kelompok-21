219eafa7e3da6edbbfa1a001fb30c3841c46a3f3
rifkiroboth <128888829+rifkiroboth@users.noreply.github.com>
2023-04-02 15:20:36 +0700

    Update tugasdkpkel20.py

--- tugasdkpkel20.py
+++ tugasdkpkel20.py
@@ -7,3 +7,10 @@ class Counter:
     def _init_(self):
         self.vowels = 0
         self.consonants = 0
+def count(self, text):
+        for char in text:
+            if char.isalpha():
+                if is_vowel(char):
+                    self.vowels += 1
+                else:
+                    self.consonants += 1
